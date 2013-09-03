dad9e5fcac9325bdf0afc2d9af4ce388188d06b4
Predrag Knezevic <predrag.knezevic@abz-reporting.com>
2013-09-03 15:01:11 +0200

    added build dependency to rest-client-builder:1.0.3, as suggested on the release plugin page.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -14,7 +14,7 @@ grails.project.dependency.resolution = {
  
     }
     plugins {
-		build (':release:2.2.1') {
+		build (':release:2.2.1', ':rest-client-builder:1.0.3') {
             export = false
         }
 		test(':spock:0.7') {
