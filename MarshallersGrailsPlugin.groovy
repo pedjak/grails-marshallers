9c8d8e172ea5347a65e396613829a64e851e581d
Angel Ruiz <aruiz@nerderg.com>
2014-04-10 16:35:45 +1000

    Prepare next development version 0.6-SNAPSHOT

--- MarshallersGrailsPlugin.groovy
+++ MarshallersGrailsPlugin.groovy
@@ -29,7 +29,7 @@ import grails.converters.JSON;
 
 class MarshallersGrailsPlugin {
 	// the plugin version
-    def version = "0.5.1"
+    def version = "0.6-SNAPSHOT"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.3 > *"
     
