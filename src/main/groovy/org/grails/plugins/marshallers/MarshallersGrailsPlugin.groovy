483f61df30e2e1d0142651f3b9bc6798440f7914
mansiarora <mansi@intelligrape.com>
2016-10-18 11:36:04 +0530

    Moved plugin descriptor to appropriate package; Updated plugin descriptor w.r.t grails-3;

--- src/main/groovy/org/grails/plugins/marshallers/MarshallersGrailsPlugin.groovy
+++ src/main/groovy/org/grails/plugins/marshallers/MarshallersGrailsPlugin.groovy
@@ -1,18 +1,21 @@
+package org.grails.plugins.marshallers
 /*******************************************************************************
  * Copyright 2011 Predrag Knezevic
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  ******************************************************************************/
+
+import grails.plugins.Plugin
 import org.codehaus.groovy.grails.commons.GrailsClassUtils as GCU;
 import org.codehaus.groovy.grails.support.proxy.DefaultProxyHandler;
 import org.codehaus.groovy.grails.support.proxy.ProxyHandler;
@@ -27,36 +30,34 @@ import grails.converters.XML;
 import grails.converters.JSON;
 
 
-class MarshallersGrailsPlugin {
-	// the plugin version
-    def version = "0.7"
+class MarshallersGrailsPlugin extends Plugin {
     // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "2.3 > *"
-    
-    def dependsOn = [converters: grailsVersion]
-      
+    def grailsVersion = "3.2.0 > *"
     // resources that are excluded from plugin packaging
+
+    def dependsOn = [converters: grailsVersion]
+
     def pluginExcludes = [
             "grails-app/views/error.gsp"
     ]
 
     def scm = [url: "https://github.com/pedjak/grails-marshallers"]
     def licence = "APACHE"
-    
-	def artefacts = [
-		XmlMarshallerArtefactHandler,
-		JsonMarshallerArtefactHandler
-	]
+
+    def artefacts = [
+            XmlMarshallerArtefactHandler,
+            JsonMarshallerArtefactHandler
+    ]
 
     def watchedResources = [
-        'file:./grails-app/domain/*.groovy',
-        'file:./grails-app/conf/Config.groovy'
+            'file:./grails-app/domain/*.groovy',
+            'file:./grails-app/conf/Config.groovy'
     ]
-    
+
     def author = "Predrag Knezevic"
     def authorEmail = "pedjak@gmail.com"
-    
-     def developers = [[name: "Denis Halupa", email: "denis.halupa@gmail.com"], [name: "Angel Ruiz", email: "aruizca@gmail.com"]]
+
+    def developers = [[name: "Denis Halupa", email: "denis.halupa@gmail.com"], [name: "Angel Ruiz", email: "aruizca@gmail.com"], [name: "Mansi Arora", email: "mansi.arora@tothenew.com"]]
 
     def title = "Easy Custom XML and JSON Marshalling for Grails Converters"
     def description = '''\\
@@ -69,7 +70,7 @@ Further documentation can be found on the GitHub repo.
     def documentation = "https://github.com/pedjak/grails-marshallers"
 
     def doWithSpring = {
-        
+
         extendedConvertersConfigurationInitializer(ExtendedConvertersConfigurationInitializer)
         ["xml", "json"].each { type ->
             application."${type}MarshallerClasses".each { marshallerClass ->
@@ -79,11 +80,11 @@ Further documentation can be found on the GitHub repo.
             }
         }
     }
-	
-	def doWithDynamicMethods = {applicationContext ->
-		applicationContext.extendedConvertersConfigurationInitializer.initialize()
-		log.debug "Marshallers Plugin configured successfully"
-	}
+
+    def doWithDynamicMethods = { applicationContext ->
+        applicationContext.extendedConvertersConfigurationInitializer.initialize()
+        log.debug "Marshallers Plugin configured successfully"
+    }
 
     def onChange = { event ->
         event.ctx.extendedConvertersConfigurationInitializer.initialize()
